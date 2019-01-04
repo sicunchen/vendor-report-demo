dc6d8b309c47464763c46148011d604739a1b3f7
Lia Chen <scunchen@ad.isr.umich.edu>
2019-01-04 16:25:17 -0500

    Updates

---
+++ precache-manifest.a57a9fa614bf24b90464f0b5a4c9137d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d879b0f89c5234944f90",
+    "url": "/vendor-report-demo/static/js/runtime~main.d879b0f8.js"
+  },
+  {
+    "revision": "b9c038879947210dbc84",
+    "url": "/vendor-report-demo/static/js/main.b9c03887.chunk.js"
+  },
+  {
+    "revision": "20f99ac5f84e5cb7a7f1",
+    "url": "/vendor-report-demo/static/js/1.20f99ac5.chunk.js"
+  },
+  {
+    "revision": "b9c038879947210dbc84",
+    "url": "/vendor-report-demo/static/css/main.c7c6ef32.chunk.css"
+  },
+  {
+    "revision": "6cbc54ac1a8b3a9124e6b9ffe5a1e523",
+    "url": "/vendor-report-demo/index.html"
+  }
+];
